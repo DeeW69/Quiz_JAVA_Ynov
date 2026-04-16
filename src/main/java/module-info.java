1d4eebbd97a24824eeaaba19a803ad3941ebcba0
DeeW69 <30826575+DeeW69@users.noreply.github.com>
2026-04-16 16:25:14 +0200

    init new window

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.quiz_java {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.quiz_java to javafx.fxml;
+    exports com.example.quiz_java;
+}
